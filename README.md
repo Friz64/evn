784329fe85887f443a3745dbac5f1bc624e64a69
friz64 <friz64@tumbleweed.speedport.ip>
2019-02-03 02:40:22 +0100

    Added physical and logical devices

--- README.md
+++ README.md
@@ -14,26 +14,19 @@ System for loading external files asynchronously.
     - Meant to change (Configs, ...)
     - Packed path: `./`
 
-## Shader compilation
-
-Done with evn_shaderc, a command line wrapper around [shaderc](https://github.com/google/shaderc-rs).
-
-- ### Usage
-
-`./compile_shaders.sh`
-
-#### This will
-
-- Compile evn_shaderc in release mode
-- Run evn_shaderc with the correct arguments
-  - Compiles GLSL shaders in `evn/src/shaders/`
-  - Saves them in `resources/closed/shaders/`
-
-#### Requirements
-
-- Linux (maybe provide bat script for windows?)
-
 ## Building / Running
+
+```
+USAGE:
+    evn [FLAGS]
+
+FLAGS:
+        --debug-callback    Enable Vulkan debug callback
+        --dev               Enable Development mode
+    -h, --help              Prints help information
+    -c, --no-color          Don't color the console log
+    -V, --version           Prints version information
+```
 
 - ### Development Mode
 
@@ -53,3 +46,22 @@ Done with evn_shaderc, a command line wrapper around [shaderc](https://github.co
 
 - Linux (maybe provide bat script for windows?)
 - A [windows cross compiler](https://github.com/japaric/rust-cross/blob/master/README.md) when compiling to windows
+
+## Shader compilation
+
+Done with evn_shaderc, a command line wrapper around [shaderc](https://github.com/google/shaderc-rs).
+
+- ### Usage
+
+`./compile_shaders.sh`
+
+#### This will
+
+- Compile evn_shaderc in release mode
+- Run evn_shaderc with the correct arguments
+  - Compiles GLSL shaders in `evn/src/shaders/`
+  - Saves them in `resources/closed/shaders/`
+
+#### Requirements
+
+- Linux (maybe provide bat script for windows?)
--- evn_engine/src/rendering/mod.rs
+++ evn_engine/src/rendering/mod.rs
@@ -2,10 +2,14 @@ mod platform;
 pub mod shaders;
 
 use ash::{
-    extensions::ext::DebugUtils,
+    extensions::{
+        ext::DebugUtils,
+        khr::{Surface, Swapchain},
+    },
     version::{DeviceV1_0, EntryV1_0, InstanceV1_0},
     vk, vk_make_version, Device, Entry, Instance, InstanceError, LoadingError,
 };
+use either::Either;
 use err_derive::Error;
 use log::{error, info, warn};
 use specs::System;
@@ -41,6 +45,9 @@ unsafe extern "system" fn vulkan_debug_callback(
         vk::DebugUtilsMessageSeverityFlagsEXT::ERROR => {
             error!("Vulkan: {}", message);
         }
+        vk::DebugUtilsMessageSeverityFlagsEXT::INFO => {
+            info!("Vulkan: {}", message);
+        }
         _ => (),
     };
 
@@ -55,6 +62,12 @@ pub enum RendererInitError {
     InstanceError { err: String },
     #[error(display = "Failed to create Debug Callback: {}", err)]
     DebugCallbackError { err: vk::Result },
+    #[error(display = "Failed to create Surface: {}", err)]
+    SurfaceCreationError { err: vk::Result },
+    #[error(display = "Physical Device Error: {}", err)]
+    PhysicalDeviceError { err: Either<vk::Result, String> },
+    #[error(display = "Failed to create Device: {}", err)]
+    DeviceCreationError { err: vk::Result },
 }
 
 pub struct Renderer {
@@ -64,6 +77,12 @@ pub struct Renderer {
     entry: Entry,
     instance: Instance,
     debug: Option<(DebugUtils, vk::DebugUtilsMessengerEXT)>,
+    surface: vk::SurfaceKHR,
+    surface_loader: Surface,
+    physical_device: vk::PhysicalDevice,
+    device: Device,
+    graphics_queue: vk::Queue,
+    present_queue: vk::Queue,
 }
 
 impl Renderer {
@@ -73,13 +92,13 @@ impl Renderer {
                 LoadingError::LibraryLoadError(err) => RendererInitError::LoadingError { err },
             })?;
 
-            let mut layer_names = Vec::new();
-            let mut extension_names = platform::extension_names();
+            let mut instance_layer_names = Vec::new();
+            let mut instance_extension_names = platform::extension_names();
 
             if debug_callback {
-                layer_names.push(string_pointer("VK_LAYER_LUNARG_standard_validation"));
+                instance_layer_names.push(string_pointer("VK_LAYER_LUNARG_standard_validation"));
 
-                extension_names.push(string_pointer("VK_EXT_debug_utils"));
+                instance_extension_names.push(string_pointer("VK_EXT_debug_utils"));
             }
 
             let application_name = CString::new("evn").unwrap();
@@ -93,8 +112,8 @@ impl Renderer {
 
             let instance_create_info = vk::InstanceCreateInfo::builder()
                 .application_info(&app_info)
-                .enabled_layer_names(&layer_names)
-                .enabled_extension_names(&extension_names);
+                .enabled_layer_names(&instance_layer_names)
+                .enabled_extension_names(&instance_extension_names);
 
             let instance = entry
                 .create_instance(&instance_create_info, None)
@@ -111,6 +130,7 @@ impl Renderer {
                 let debug_create_info = vk::DebugUtilsMessengerCreateInfoEXT::builder()
                     .message_severity(
                         vk::DebugUtilsMessageSeverityFlagsEXT::VERBOSE
+                            //| vk::DebugUtilsMessageSeverityFlagsEXT::INFO
                             | vk::DebugUtilsMessageSeverityFlagsEXT::WARNING
                             | vk::DebugUtilsMessageSeverityFlagsEXT::ERROR,
                     )
@@ -131,11 +151,109 @@ impl Renderer {
                 None
             };
 
+            let surface = platform::create_surface(&entry, &instance, &window)
+                .map_err(|err| RendererInitError::SurfaceCreationError { err })?;
+            let surface_loader = Surface::new(&entry, &instance);
+
+            // maybe provide gui for device selection down the line?
+            let (physical_device, graphics_family_index, present_family_index) = instance
+                .enumerate_physical_devices()
+                .map_err(|err| RendererInitError::PhysicalDeviceError {
+                    err: Either::Left(err),
+                })?
+                .into_iter()
+                .filter_map(|physical_device| {
+                    let queue_family_properties =
+                        instance.get_physical_device_queue_family_properties(physical_device);
+
+                    let graphics_family_index = queue_family_properties
+                        .iter()
+                        .enumerate()
+                        .filter_map(|(index, properties)| {
+                            let support = properties.queue_flags.contains(vk::QueueFlags::GRAPHICS);
+
+                            if support {
+                                Some(index as u32)
+                            } else {
+                                None
+                            }
+                        })
+                        .nth(0);
+
+                    let present_family_index = (0..queue_family_properties.len())
+                        .filter_map(|index| {
+                            let support = surface_loader.get_physical_device_surface_support(
+                                physical_device,
+                                index as u32,
+                                surface,
+                            );
+
+                            if support {
+                                Some(index as u32)
+                            } else {
+                                None
+                            }
+                        })
+                        .nth(0);
+
+                    match (graphics_family_index, present_family_index) {
+                        (Some(graphics), Some(present)) => {
+                            Some((physical_device, graphics, present))
+                        }
+                        _ => None,
+                    }
+                })
+                .nth(0)
+                .ok_or(RendererInitError::PhysicalDeviceError {
+                    err: Either::Right("Failed to find suitable device".into()),
+                })?;
+
+            let device_extension_names = [Swapchain::name().as_ptr()];
+            let mut device_layer_names = Vec::new();
+            let device_features = vk::PhysicalDeviceFeatures::builder();
+
+            if debug_callback {
+                device_layer_names.push(string_pointer("VK_LAYER_LUNARG_standard_validation"));
+            }
+
+            let mut queue_family_indices = vec![graphics_family_index, present_family_index];
+            // remove duplicates
+            queue_family_indices.sort();
+            queue_family_indices.dedup();
+            let device_queue_create_info = queue_family_indices
+                .into_iter()
+                .map(|queue_family_index| {
+                    vk::DeviceQueueCreateInfo::builder()
+                        .queue_family_index(queue_family_index)
+                        .queue_priorities(&[1.0])
+                        .build()
+                })
+                .collect::<Vec<_>>();
+
+            let device_create_info = vk::DeviceCreateInfo::builder()
+                .queue_create_infos(&device_queue_create_info)
+                .enabled_extension_names(&device_extension_names)
+                .enabled_layer_names(&device_layer_names)
+                .enabled_features(&device_features);
+
+            let device = instance
+                .create_device(physical_device, &device_create_info, None)
+                .map_err(|err| RendererInitError::DeviceCreationError { err })?;
+
+            let graphics_queue = device.get_device_queue(graphics_family_index, 0);
+            let present_queue = device.get_device_queue(present_family_index, 0);
+
             Ok(Renderer {
                 window,
                 entry,
                 instance,
                 debug,
+                surface,
+                surface_loader,
+                physical_device,
+                device,
+                graphics_queue,
+                present_queue,
             })
         }
     }
@@ -150,6 +268,10 @@ impl<'a> System<'a> for Renderer {
 impl Drop for Renderer {
     fn drop(&mut self) {
         unsafe {
+            self.surface_loader.destroy_surface(self.surface, None);
+
+            self.device.destroy_device(None);
+
             if let Some((debug_utils, debug)) = &self.debug {
                 debug_utils.destroy_debug_utils_messenger(*debug, None);
             }
